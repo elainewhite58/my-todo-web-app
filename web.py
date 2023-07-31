8d40618acb13f753a04b84bded365c37e45b2e61
Elaine <elainewhite315@gmail.com>
2023-07-31 20:09:00 +0100

    Initial commit

--- web.py
+++ web.py
@@ -60,3 +60,11 @@ st.text_input(label="", placeholder="Add new todo...",
 # pip freeze > requirements.txt to set up the file (it automatically writes the
 # 3rd party packages to the txt file.
 # pip freeze on it's own writes the packages in the command line.
+
+
+## following setting up a streamlit account and linking to github
+# the app is deployed with this link:
+# https://my-todo-web-appg-8p8zuvbye7ponpzsh4ghvb.streamlit.app
+# click on manage app to see errors under the hood directly
+
+# streamlit gives you the deployment facility and cloud server
